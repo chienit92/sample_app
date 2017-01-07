a7e766efe083c471a3ef8d3920499201a5780bb6
ringo92 <nd.chien.it@gmail.com>
2017-01-07 11:04:09 +0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by id: params[:id]
-    if @user.nil?
-      flash[:notice] = "Don't Have Users, Please create"
+    if @user.present?
+      flash[:danger] = "Don't Have Users, Please create"
       redirect_to signup_path
     end
   end
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
