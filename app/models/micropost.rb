73573cf718db580758f3a26c0c742e9d555f6910
ringo92 <nd.chien.it@gmail.com>
2017-01-17 13:43:40 +0700

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,9 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  scope :feeds_list, -> user_id{where user_id: user_id}
   scope :order_by_time, -> {order created_at: :desc}
   mount_uploader :picture, PictureUploader
+
   validates :user, presence: true
   validates :content, presence: true, length: {maximum: 140}
   validate  :picture_size
